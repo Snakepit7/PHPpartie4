157d2f02e42068b974bfb033967ad01c765f7e3a
Snakepit7 <julienfollet71@gmail.com>
2018-11-19 09:48:31 +0100

    exos

---
+++ exo2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo2</title>
+  </head>
+  <body>
+      <?php
+        function text(){
+            echo 'C\'est une chaine de caractères.'; 
+        }
+        
+        text();
+      ?>
+  </body>
+</html>
---
+++ exo3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo3</title>
+  </head>
+  <body>
+      <?php
+      /**
+       * création de la fonction de type "string".
+       */
+        function text(){
+            echo 'C\'est une chaine de caractères.'.' Suivie d\'une autre chaine de cractères'; 
+        }
+        
+        /**
+         * Appel de la fonction afin de l'afficher dans le navigateur.
+         */
+        text();
+      ?>
+  </body>
+</html>
