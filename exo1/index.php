031c5b0554ecb944a3d37c7f7a4efa81344bab08
Snakepit7 <julienfollet71@gmail.com>
2018-11-19 09:33:01 +0100

    exos

---
+++ exo1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function truefunc($string) {
+    return $string;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo1</title>
+  </head>
+  <body>
+      <?php
+        echo truefunc('true');
+      ?>
+  </body>
+</html>
